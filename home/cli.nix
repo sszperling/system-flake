937cdc2ab09faaaadd7bda2fe562e33e3d0c5c9c
Safiro Szperling <zebaszp@gmail.com>
2026-03-06 12:35:13 +0100

    clean up cli

--- home/cli.nix
+++ home/cli.nix
@@ -31,9 +31,6 @@
     ];
 
     initContent = lib.mkBefore ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
@@ -42,8 +39,6 @@
     shellAliases = {
       ll = "ls -l";
     };
-
-    history.size = 10000;
   };
 
   programs.git = {
