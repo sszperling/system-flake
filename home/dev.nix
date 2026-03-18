0c1750ca535c58265544aef500343875a7784907
Safiro Szperling <zebaszp@gmail.com>
2026-03-18 07:55:58 +0100

    add android dev tools

--- home/dev.nix
+++ home/dev.nix
@@ -3,5 +3,9 @@
 {
   home.packages = with pkgs; [
     vscodium
+  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+    android-studio # only provided for linux
+    android-tools # no point in installing without the above
+    gradle # no point in installing without the above
   ];
 }
