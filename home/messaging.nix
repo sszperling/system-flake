5f85911ac67cb24193d288fed810e4ab6d4fa7a8
Safiro Szperling <zebaszp@gmail.com>
2026-03-18 10:28:45 +0100

    add whatsapp

--- home/messaging.nix
+++ home/messaging.nix
@@ -6,5 +6,8 @@
   ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
     element-desktop # does not build on mac
     telegram-desktop # use mac specific version instead
+    whatsapp-electron # not provided for mac, use specific version instead
+  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isMacOS [
+    # whatsapp-for-mac # broken
   ];
 }
