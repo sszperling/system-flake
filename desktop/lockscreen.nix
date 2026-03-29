a28450344830e2e9b6ec05553d35eb27fefd2afe
Safiro Szperling <zebaszp@gmail.com>
2026-03-29 14:15:39 +0200

    update hyprlock config and wallpaper for more better catppuccin

--- desktop/lockscreen.nix
+++ desktop/lockscreen.nix
@@ -1,7 +1,93 @@
 { pkgs, ... }:
 
 {
-  programs.hyprlock.enable = true;
+  catppuccin.hyprlock.useDefaultConfig = false;
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      "$font" = "JetBrainsMono Nerd Font";
+
+      # GENERAL
+      general = {
+        hide_cursor = true;
+      };
+
+      # BACKGROUND
+      background = {
+        monitor = "";
+        path = builtins.path { path =  ./wallpaper.png; };
+        blur_passes = 0;
+        color = "$base";
+      };
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME";
+          color = "$text";
+          font_size = 90;
+          font_family = "$font";
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = "cmd[update:43200000] date +'%A, %d %B %Y'";
+          color = "$text";
+          font_size = 25;
+          font_family = "$font";
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = "$FPRINTPROMPT";
+          color = "$text";
+          font_size = 14;
+          font_family = "$font";
+          position = "0, -107";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      # USER AVATAR
+      #image = [{
+      #  monitor = "";
+      #  path = ./face;
+      #  size = 100;
+      #  border_color = "$accent"
+      #  position = "0, 75";
+      #  halign = "center";
+      #  valign = "center";
+      #}]
+
+      input-field = [{
+        monitor = "";
+        size = "300, 60";
+        outline_thickness = 4;
+        dots_size = 0.2;
+        dots_spacing = 0.2;
+        dots_center = true;
+        outer_color = "$accent";
+        inner_color = "$surface0";
+        font_color = "$text";
+        fade_on_empty = false;
+        placeholder_text = "<span foreground=\"##$textAlpha\"><i>󰌾 Logged in as </i><span foreground=\"##$accentAlpha\">$USER</span></span>";
+        hide_input = false;
+        check_color = "$accent";
+        fail_color = "$red";
+        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+        capslock_color = "$yellow";
+        position = "0, -47";
+        halign = "center";
+        valign = "center";
+      }];
+    };
+  };
 
   services.hypridle = {
     enable = true;
