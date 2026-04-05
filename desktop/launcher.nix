e370d4a51d6abd945229cb0bc8fb2df67486d91d
Safiro Szperling <zebaszp@gmail.com>
2026-04-05 15:45:40 +0200

    replace vicinae with ulauncher

--- desktop/launcher.nix
+++ desktop/launcher.nix
@@ -1,15 +1,36 @@
 { pkgs, username, ... }:
 
 {
-  programs.vicinae = {
-    enable = true;
-    systemd.enable = true;
-  };
-
-  # hack to fix not being able to launch due to missing path
-  systemd.user.services.vicinae.Service.Environment = [
-    "PATH=/etc/profiles/per-user/${username}/bin"
+  home.packages = with pkgs; [
+    ulauncher
   ];
 
-  wayland.windowManager.sway.config.menu = "${pkgs.vicinae}/bin/vicinae open";
+  systemd.user.services.ulauncher = {
+    Unit = {
+      Description = "Linux Application Launcher";
+      Documentation = "https://ulauncher.io/";
+    };
+    Service = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 1;
+      ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
+      Environment = [
+        "PATH=/etc/profiles/per-user/${username}/bin"
+      ];
+    };
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
+
+  wayland.windowManager.sway.config = {
+    menu = "${pkgs.ulauncher}/bin/ulauncher-toggle";
+    window.commands = [
+      {
+        command = "border none";
+        criteria = { app_id = "ulauncher"; };
+      }
+    ];
+  };
 }
