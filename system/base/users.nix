5c0a5ea391de80f79ad29b8a582df74dc5cc0f38
Safiro Szperling <zebaszp@gmail.com>
2026-04-12 15:24:53 +0200

    add group and pin ids

--- system/base/users.nix
+++ system/base/users.nix
@@ -1,11 +1,19 @@
 { pkgs, username, homedir, ... }:
 
 {
-  users.users.${username} = {
-    isNormalUser = true;
-    description = "Safiro Szperling";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
+  users = {
+    groups.${username} = {
+      gid = 1000;
+      members = [ username ];
+    };
+    users.${username} = {
+      isNormalUser = true;
+      description = "Safiro Szperling";
+      uid = 1000;
+      group = username;
+      extraGroups = [ "networkmanager" "wheel" ];
+      shell = pkgs.zsh;
+    };
   };
 
   home-manager.users.${username} = {};
