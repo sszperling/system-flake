5b044d76cba9491c21f21e52e7bbff64f002f524
Safiro Szperling <zebaszp@gmail.com>
2026-04-12 16:14:29 +0200

    add recommended tailscale config

--- system/server/networking.nix
+++ system/server/networking.nix
@@ -1,7 +1,24 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.tailscale = {
     useRoutingFeatures = "server";
   };
+
+  networking.nftables.enable = true;
+  networking.firewall = {
+    # Always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+    # Allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+  # Force tailscaled to use nftables (Critical for clean nftables-only systems)
+  # This avoids the "iptables-compat" translation layer issues.
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
+  ];
+
+  # Optimization: Prevent systemd from waiting for network online
+  systemd.network.wait-online.enable = false;
+  boot.initrd.systemd.network.wait-online.enable = false;
 }
