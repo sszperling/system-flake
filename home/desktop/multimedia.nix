6ee1ef5985f1467f8dd379f7e523ed7a3f742ec8
Safiro Szperling <zebaszp@gmail.com>
2026-04-27 11:35:30 +0200

    add jellyfin desktop

--- home/desktop/multimedia.nix
+++ home/desktop/multimedia.nix
@@ -10,6 +10,7 @@
   ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
     vlc # not provided for mac, use bin instead
     gimp # not provided for mac
+    jellyfin-desktop # broken on mac
   ] ++ lib.optionals pkgs.stdenv.hostPlatform.isMacOS [
     vlc-bin
   ];
